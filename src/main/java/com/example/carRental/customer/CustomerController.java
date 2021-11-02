fe72c43f5fea19851b83423733f28867f10bd6d8
johngillard15 <johngillard15@gmail.com>
2021-11-02 17:24:57 -0400

    finished customer entity and controller

--- src/main/java/com/example/carRental/customer/CustomerController.java
+++ src/main/java/com/example/carRental/customer/CustomerController.java
@@ -12,56 +12,62 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-//    private final AtomicLong customerId = new AtomicLong();
-//    private Map<Long, Customer> customers = new HashMap<>();
-//
-//    public CustomerController(){
-//        long id = customerId.incrementAndGet();
-//        customers.put(id, new Customer(id, "Phred", "Phlintstone"));
-//    }
-//
-//    @GetMapping
-//    public String root(){
-//        return "customer root directory";
-//    }
-//
-//    @GetMapping("/all")
-//    public List<Customer> all() {
-//        return new ArrayList<>(customers.values());
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Customer customer(@PathVariable long id) {
-//        return customers.get(id);
-//    }
-//
-//    @PostMapping
-//    public Customer newCustomer(@RequestBody Customer newCustomer) {
-//        long id = customerId.incrementAndGet();
-//
-//        newCustomer.setId(id);
-//        customers.put(id, newCustomer);
-//
-//        return newCustomer;
-//    }
-//
-//    @PutMapping("/{id}")
-//    public Customer updateCar(@PathVariable long id, @RequestBody Customer newCustomer){
-//        Customer thiscustomer = customers.get(id);
-//
-//        if(thiscustomer == null)
-//            return thiscustomer;
-//
-//        if(newCar.getMake() != null)
-//            thiscustomer.setMake(newCar.getMake());
-//        if(newCar.getModel() != null)
-//            thiscustomer.setModel(newCar.getModel());
-//
-//        return thiscustomer;
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deleteCar(@PathVariable long id){
-//        cars.remove(id);
-//    }
+    private final AtomicLong customerId = new AtomicLong();
+    private Map<Long, Customer> customers = new HashMap<>();
+
+    public CustomerController(){
+        long id = customerId.incrementAndGet();
+        customers.put(id, new Customer(id, "Phred", "Phlintstone"));
+
+        id = customerId.incrementAndGet();
+        customers.put(id, new Customer(id, "John", "Gillard"));
+
+        id = customerId.incrementAndGet();
+        customers.put(id, new Customer(id, "First", "Last"));
+    }
+
+    @GetMapping
+    public String root(){
+        return "customer root directory";
+    }
+
+    @GetMapping("/all")
+    public List<Customer> all() {
+        return new ArrayList<>(customers.values());
+    }
+
+    @GetMapping("/{id}")
+    public Customer customer(@PathVariable long id) {
+        return customers.get(id);
+    }
+
+    @PostMapping
+    public Customer newCustomer(@RequestBody Customer newCustomer) {
+        long id = customerId.incrementAndGet();
+
+        newCustomer.setId(id);
+        customers.put(id, newCustomer);
+
+        return newCustomer;
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCar(@PathVariable long id, @RequestBody Customer newCustomer){
+        Customer thiscustomer = customers.get(id);
+
+        if(thiscustomer == null)
+            return thiscustomer;
+
+        if(newCustomer.getFirstName() != null)
+            thiscustomer.setFirstName(newCustomer.getFirstName());
+        if(newCustomer.getLastName() != null)
+            thiscustomer.setLastName(newCustomer.getLastName());
+
+        return thiscustomer;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable long id){
+        customers.remove(id);
+    }
 }
